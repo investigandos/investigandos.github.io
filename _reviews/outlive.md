abf157be1bcc53e1258d3a27609e3f1d3d549d48
Tovarlogic <tovarlogic@gmail.com>
2024-08-11 09:52:17 +0100

    new content + removed unneeded JS + improved article and complex layouts

---
+++ _reviews/outlive.md
@@ -0,0 +1,34 @@
+---
+book: 9781785044540
+author: Carlos
+subtitle: the science and art of longevity
+date: 2024-08-10
+draft: true
+---
+
+# Propósito del libro
+
+El **objetivo** es informar sobre como «vivir más y mejor», es decir, sin enfermedades ni molestias, manteniendo el ánimo o felicidad. Como retrasar el declive que se da en la vejez y acortar el periodo de deterioro hasta que llegue la muerte. 
+
+Para ello establece recomienda establecer una **estrategia** y ciertas **tácticas** basadas en el conocimiento científico. El autor hace hincapié en que no se trata de medidas *basadas en evidencia científica* en el sentido más estricto ya que reconoce que el conocimiento actual es aún muy limitado. Intenta, al menos, ir en la «dirección correcta» y evitar la inacción hasta que esta llegue. 
+
+En definitiva, trata de explicar las razones conocidas que evitan la enfermedad y mejoran la salud a pesar de los años; para, en consecuencia, establecer pequeños cambios de hábitos con efecto positivo acumulable a lo largo de la vida.
+
+# Síntesis
+
+## Introducción (Cap. 1 a 3)
+La visión del médico sobre la longevidad pasa por un cambio de paradigma en la medicina, pasando de la actual práctica, a la que llama «Medicina 2.0» a una con un enfoque más *proactivo*, más enfocada a la prevención, que llama «Medicina 3.0».   
+
+Al comienzo del libro describe cuáles son sus principales fuentes de información, que clasifica como:
+- Estudios observacionales en personas centenarias
+- Modelos animales
+- Estudios en humanos sobre las cuatro grandes enfermedades o «jinetes del Apocalipsis» (Diabetes, Cáncer, CVD y Alzheimer).
+- Estudios, en humanos y animales, sobre los mecanismos del envejecimiento a nivel molecular
+
+Lamentablemente, aunque incluye una buena colección bibliográfica, principalmente estudios científicos, no hace hace referencia directa a los mismos, a lo largo de la lectura, lo que dificulta su uso.
+
+## Evidencias (Cap. 4 a 9)
+
+## Acciones (Cap. 10 a 17)
+
+# Dudas sin responder
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,13 @@
-(function($, List, _, moment) {
+(function($, List, _) {
 
   // List.js classes to use for search elements
-  var listOptions = {
-    valueNames: [
-      'list-text',
-      'list-tag',
-      'list-added_by',
-      'list-status',
-      'list-author',
-      'list-year'
-    ]
-  };
-
   var options = {
-    valueNames: ['tags', 'authors', 'text', 'consulted_date', 'published_year', 'details']
+    valueNames: ['list-tags', 'list-text', 'consulted_date', 'published_year', 'list-details']
   };
   var booksList = new List('book', options);
   var mediaList = new List('media', options);
   var articlesList = new List('article', options);
   var papersList = new List('paper', options);
-
 
   $(".search").keyup(function(){
     booksList.search($(this).val());
@@ -28,56 +16,8 @@
     papersList.search($(this).val());
   });
 
-  // tooltip
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  });
-
-  // tabs
-  $('#myTabs a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-  });
-
-  // Find any within a facet
-  function foundAny(facets, compareItem) {
-    
-    // No facets selected, show all for this facet
-    if (_.isEmpty(facets)) {
-      return true;
-    }
-    // Otherwise, show this item if it contains any of the selected facets
-    return facets.reduce(function(found, facet) {
-      if (found) {
-        return found;
-      }
-
-      var tryThis = compareItem[facet['facet']].split('; ');
-      for (var i=0, j=tryThis.length; i<j; i++) {
-        if (tryThis[i].toLowerCase().replace(/\s/g, "") == facet['value'].toLowerCase().replace(/\s/g, "")) {
-          return true;
-        }
-      }
-      return false;
-    }, false);
-  }
 
   $(function() {
-
-    // Dates
-    var today = moment();
-    //var inauguration = moment(document.getElementById("inauguration").value);
-    //var inauguration = moment('2017-01-20');
-    //var from = $('#inauguration')[0].value;
-    //var inauguration = moment(from);
-    //$('#inauguration-days').html(inauguration.diff(today, 'days') > 0 ? inauguration.diff(today, 'days') : 'NA');
-    
-    //$('#days-in-office').html(today.diff(inauguration, 'days') > 0 ? today.diff(inauguration, 'days') : 0);
-
-    // List.js object that we can filter upon
-    var promiseList = new List('promises', listOptions).on('updated', function(list) {
-      $('#count').html(list.visibleItems.length);
-    });
 
     var $search = $('#search');
     var $facets = $('[data-list-facet]'); // All buttons that can filter
@@ -94,63 +34,6 @@
       promiseList.filter();
     }
 
-    // Hard reset all the buttons
-    $('.list-filter-reset').on('click', resetFilter);
-
-    // Any facet filter button
-    $facets.on('click', function(e) {
-
-      var facet = $(this).data('list-facet'); // ie 'list-category or list-status'
-      var value = $(this).data('facet-value'); // ie 'Culture'
-      var isSingle = !!$(this).data('select-single'); // ie true/false for if there can only be one of this filter
-      
-      // Single-select categories should have their active state wiped
-      if (isSingle) {
-        $facets
-          .filter(function() { return $(this).data('list-facet') === facet; })
-          .removeClass('active');
-      }
-
-      // Flag as active
-      $(this).toggleClass('active');
-
-      // Array of active
-      var facets = $facets.filter('.active').map(function() {
-        // return object instead with facet/value
-        return {
-          facet: $(this).data('list-facet'),
-          value: $(this).data('facet-value'),
-          isSingle: !!$(this).data('select-single')
-        };
-      }).get();
-      
-
-      // When deselecting last, clear all filters
-      if (facets.length === 0) {
-        promiseList.filter();
-        return; // Eject now
-      }
-
-      // Otherwise, filter on the array
-      promiseList.filter(function(item) {
-
-        var itemValues = item.values();
-        
-        // Single selects, eg "Not started"
-        var single = _.filter(facets, ['isSingle', true]);
-        var foundSingle = foundAny(single, itemValues);
-        // Single-selection items hide if false no matter what, so eject if not found here
-        if (!foundSingle) {
-          return false;
-        }
-
-        // Full categories can have multiples show, list out here
-        var multis = _.filter(facets, ['isSingle', false]);
-        return foundAny(multis, itemValues);
-
-      }); // promiseList.filter()
-
-    });
   });
 
-})(jQuery, List, _, moment);
+})(jQuery, List, _);
